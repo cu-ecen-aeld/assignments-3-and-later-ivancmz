94b4cb3c9ca967c2365eff411c4b57cfe1f448dc
Ivan Marquez <ivancmz@gmail.com>
2026-02-23 09:38:51 +0100

    Assignment 4 part 1: Threading

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -13,7 +13,17 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    if (pthread_mutex_lock(thread_func_args->mutex) != 0) {
+        perror("Failed to obtain mutex");
+        ERROR_LOG("Failed to obtain mutex");
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+    pthread_mutex_unlock(thread_func_args->mutex);
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +38,25 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data* thread_data_ptr = (struct thread_data*)malloc(sizeof(struct thread_data));
+    if(thread_data_ptr == NULL){
+        perror("Failed to allocate memory for thread data");
+        ERROR_LOG("Failed to allocate memory for thread data");
+        return false;
+    }
+    thread_data_ptr->mutex = mutex;
+    thread_data_ptr->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_data_ptr->wait_to_release_ms = wait_to_release_ms;
+    thread_data_ptr->thread_complete_success = false;
+    
+    int result = pthread_create(thread, NULL, threadfunc, (void*)thread_data_ptr);
+    if(result != 0){
+        perror("Failed to create thread");
+        ERROR_LOG("Failed to create pthread");
+        free(thread_data_ptr);
+        return false;
+    }
+    
+    return true;
 }
 
